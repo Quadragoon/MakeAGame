cdf6f4d0ae7dc0b56c845cf5f546106176b91359
Viktor-Domenstam <56826979+Viktor-Domenstam@users.noreply.github.com>
2022-11-27 14:23:36 +0100

    Added a menu
    Added a menu
    Start
    Options
    Quit

---
+++ Menu.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public class Menu : Control
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public void _OnStartButtonPressed()
+    {
+        GetTree().ChangeScene("res://Game.tscn");
+    }
+
+    public void _OnOptionsButtonPressed()
+    {
+
+    }
+
+    public void _OnQuitButtonPressed()
+    {
+
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
