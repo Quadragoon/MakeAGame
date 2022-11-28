d28b4453500b2b92b519d6070f969a701f764846
Viktor-Domenstam <56826979+Viktor-Domenstam@users.noreply.github.com>
2022-11-28 16:18:04 +0100

    Removed old files

--- Player/MissileTargeted.cs
+++ Player/MissileTargeted.cs
@@ -5,21 +5,23 @@ public class MissileTargeted : Missile
 {
     private float timer = 0.0f;
     public float fireDelay;
-    private float freeFloatTime = 0.25f;
+    private float freeFloatTime = 0.5f;
     public Vector2 TargetLocation;
     private Sprite missileSprite;
     private float freeFloatRotationSpeed;
     private float defaultSpriteRotation;
-    private float turnSpeed = 10f;
+    private float turnSpeed = 3f;
+    private float acceleration = 400f;
+    public Node2D firedFrom;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         Visible = false;
-        Speed = 500;
+        Speed = 400;
         missileSprite = GetNode<Sprite>("Sprite");
         defaultSpriteRotation = missileSprite.Rotation;
-        freeFloatRotationSpeed = (GD.Randf() + 0.1f) * 10;
+        freeFloatRotationSpeed = (GD.Randf() - 0.5f) * 50;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,19 +30,28 @@ public class MissileTargeted : Missile
         if (fireDelay > 0)
         {
             fireDelay -= Mathf.Min(delta, fireDelay);
+            if (fireDelay <= 0)
+            {
+                Visible = true;
+                GlobalPosition = firedFrom.GlobalPosition;
+            }
         }
         else if (freeFloatTime > 0)
         {
-            Visible = true;
+            Speed -= acceleration * delta;
             freeFloatTime -= Mathf.Min(delta, freeFloatTime);
             missileSprite.Rotate(freeFloatRotationSpeed * delta);
             GlobalPosition += new Vector2((float)Math.Cos(GlobalRotation), (float)Math.Sin(GlobalRotation)) * Speed/2 * delta;
+            if (freeFloatTime <= 0)
+            {
+                Rotation = missileSprite.Rotation;
+                missileSprite.Rotation = defaultSpriteRotation;
+            }
         }
         else
         {
-            Visible = true;
-            missileSprite.Rotation = defaultSpriteRotation;
-            Rotate(GetAngleTo(TargetLocation) * turnSpeed * delta);
+            Speed += acceleration * delta;
+            Rotate((GetAngleTo(TargetLocation) < 0 ? -1 : 1) * turnSpeed * delta);
             timer += delta;
             GlobalPosition += new Vector2((float)Math.Cos(GlobalRotation), (float)Math.Sin(GlobalRotation)) * Speed * delta;
             if (timer >= 2.0f || GlobalPosition.DistanceTo(TargetLocation) < 5)
